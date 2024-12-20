cdb533148e17155dd39d2b4b254d38d930b365d3
jangjeonghun1004 <jangjeonghun1004@gmail.com>
2024-12-20 17:45:05 +0900

    commit: init

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
     <BrowserRouter>
       <Routes>
        <Route path='/' element={<Home/>}></Route>
+       <Route path='/React-Project1/' element={<Home/>} ></Route>
       </Routes>
     </BrowserRouter>
   )
