6240d560ffed440d8067ba03d0c6b16e07dd24d5
jangjeonghun1004 <jangjeonghun1004@gmail.com>
2024-12-21 11:28:39 +0900

    commit:init

--- src/pages/Sidebar.tsx
+++ src/pages/Sidebar.tsx
@@ -7,9 +7,11 @@ import Search from "../widgets/search/Search";
 
 function Sidebar() {
     const [isMenuOpen, setIsMenuOpen] = useState(true);
+    const [windowWidth, setWindowWidth] = useState<number>(0);
 
     const handleResize = () => {
         const width = window.innerWidth;
+        setWindowWidth(width);
 
         if(width < 768) {
             setIsMenuOpen(false);
@@ -25,11 +27,11 @@ function Sidebar() {
     useEffect(() => {
         handleResize();
 
-        //window.addEventListener('resize', handleResize);
+        window.addEventListener('resize', handleResize);
 
         // 클린업: 컴포넌트가 언마운트될 때 이벤트 리스너 제거
         return () => {
-            //window.removeEventListener('resize', handleResize);
+            window.removeEventListener('resize', handleResize);
         };
     }, []);
 
@@ -37,7 +39,7 @@ function Sidebar() {
         <div id="sidebar" className={isMenuOpen ? '' : ''}>
             <div className="inner">
                 <Search />
-                <h1>{String(isMenuOpen)}</h1>
+                <h1>{String(isMenuOpen)}, {windowWidth}</h1>
                 <Menu />
                 <AnteInterdum />
                 <GetInTouch />
