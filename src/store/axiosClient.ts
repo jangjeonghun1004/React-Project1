a069eee45d152f04e0a509909a713de49f3715e0
jangjeonghun1004 <jangjeonghun1004@gmail.com>
2025-02-11 21:57:06 +0900

    init

--- src/store/axiosClient.ts
+++ src/store/axiosClient.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:8080/api/',
+  //baseURL: 'http://localhost:8080/api/', 
+  baseURL:'https://newallsoft.shop/api/'
 });
 
 axiosClient.interceptors.request.use(
