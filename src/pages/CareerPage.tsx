f54105eef4b876f28b75dbf9526df4dbbce622aa
jangjeonghun1004 <jangjeonghun1004@gmail.com>
2025-03-04 19:36:47 +0900

    init

--- src/pages/CareerPage.tsx
+++ src/pages/CareerPage.tsx
@@ -1,5 +1,6 @@
 import Header from "../widgets/header/Header";
 import Sidebar from "./Sidebar";
+import ScrollAnimation from "../widgets/scrollAnimation/ScrollAnimation";
 
 export default function CareerPage() {
     return (
@@ -10,31 +11,36 @@ export default function CareerPage() {
 
                     <section>
                         <header className="main">
-                            <h1>✅ 2018. 04 ~ 2024. 08(6년 5개월)</h1>
+                            <h2>✅ 2018. 04 ~ 2024. 08(6년 5개월)</h2>
                         </header>
-                        <img src={`${import.meta.env.BASE_URL}images/qexpress1.png`} style={{width: "100%"}} />
-                        <img src={`${import.meta.env.BASE_URL}images/qexpress2.png`} style={{width: "100%"}} />
+                        <span className="image fit"><img src={`${import.meta.env.BASE_URL}images/qexpress-1.png`} alt="" /></span>
+                        <ScrollAnimation>
+                            <span className="image fit"><img src={`${import.meta.env.BASE_URL}images/qexpress-2.svg`} alt="" /></span>
+                        </ScrollAnimation>
                         <hr />
 
-                        <header className="main">
-                            <h2>✅  2017. 03 ~ 2018. 04 (1년 2개월)</h2>
-                        </header>
-                        <img src={`${import.meta.env.BASE_URL}images/study-english.png`} style={{width: "100%"}} />
-                        <hr />
+                        <ScrollAnimation>
+                            <header className="main">
+                                <h2>✅  2017. 03 ~ 2018. 04 (1년 2개월)</h2>
+                            </header>
+                            <span className="image fit"><img src={`${import.meta.env.BASE_URL}images/study-english.svg`} alt="" /></span>
+                            <hr />
+                        </ScrollAnimation>
 
+                        <ScrollAnimation>
+                            <header className="main">
+                                <h2>✅ 2013. 07 ~ 2016. 07 (3년 1개월)</h2>
+                            </header>
+                            <span className="image fit"><img src={`${import.meta.env.BASE_URL}images/totorosa.svg`} alt="" /></span>
+                            <hr />
+                        </ScrollAnimation>
 
-                       
-
-                        <header className="main">
-                            <h1>✅ 2016. 08 ~ 2017. 01(6개월)</h1>
-                        </header>
-
-
-
-
-{/* 
-                        <span className="image main"><img src={`${import.meta.env.BASE_URL}images/cat-revolution.jpeg`} style={{ height: 300 }} alt="진화" /></span>
-                        <TimeLine data={timeLineData} /> */}
+                        <ScrollAnimation>
+                            <header className="main">
+                                <h2>✅ 2006. 12 ~ 2013. 06 (6년 7개월)</h2>
+                            </header>
+                            <span className="image fit"><img src={`${import.meta.env.BASE_URL}images/tendr.svg`} alt="" /></span>
+                        </ScrollAnimation>
                     </section>
                 </div>
             </div>
